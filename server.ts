78b1d09df0dad35ed4fccc2a5f419d2bfa6e9329
Gustavo <gustavojerezt@gmail.com>
2021-05-20 21:39:24 -0500

    Website

--- server.ts
+++ server.ts
@@ -3,10 +3,13 @@ import 'zone.js/dist/zone-node';
 import { ngExpressEngine } from '@nguniversal/express-engine';
 import * as express from 'express';
 import { join } from 'path';
-
+import {enableProdMode} from '@angular/core';
 import { AppServerModule } from './src/main.server';
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
+
+// Faster server renders w/ Prod mode (dev mode never needed)
+enableProdMode();
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
